24e9168cc7a8da9265ba68893756cef14dc3a1ba
Abhay Thakur <abhaysinghthakur112212@gmail.com>
2026-03-15 04:23:51 +0530

    Correcting the cache by explicitly defining it

--- src/main/java/com/shipping/ShippingEstimatorApplication/ShippingEstimatorApplication.java
+++ src/main/java/com/shipping/ShippingEstimatorApplication/ShippingEstimatorApplication.java
@@ -2,10 +2,10 @@ package com.shipping.ShippingEstimatorApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
-@EnableCaching
+
 public class ShippingEstimatorApplication {
 
 	public static void main(String[] args) {
